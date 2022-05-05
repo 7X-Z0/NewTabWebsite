e4eb3b2fb46ad570cd0b7e296d75779058a7ad13
7X_Z0 <lorenzoguzman205@gmail.com>
2022-05-05 11:34:16 -0400

    Buttons work, search bar works

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Website is running");
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://duckduckgo.com/?q=';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
